da5b330fa989c70d7cb483d2804f4a732bdb9ae3
H.N. Gibson <revhngibson@gmail.com>
2024-09-18 13:08:36 -0500

    WIP Assignment

--- exploring_python_os_module.py
+++ exploring_python_os_module.py
@@ -9,7 +9,7 @@
 import os
 
 def list_directory_contents(path):
-    with open(f"{path}") as file:
+    with open(f"{path}") as f:
         directory_list = os.listdir(path)
         print(f"The following directories and files were found in '{path}'")
         for directory in directory_list:
